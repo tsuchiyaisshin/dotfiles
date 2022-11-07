c2f6370544a37052eeeb2b55d93add8e9dcd775d
Isshin Tsuchiya <tuchiyaisshin@icloud.com>
2022-11-07 13:07:04 +0900

    fix: delete fzf

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,11 +6,6 @@ brew install mariadb-connector-c #
 brew install fzf
 brew install ripgrep
 
-# fzf
-[ -f ~/.fzf.zsh ] && .source ~/.fzf.zsh
-export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
-export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-
 DOT_FILES=()
 DOT_FILES=(${DOT_FILES[@]} .gitconfig)
 DOT_FILES=(${DOT_FILES[@]} .gitignore_global)
